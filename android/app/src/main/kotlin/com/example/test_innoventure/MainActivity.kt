9c4bdab05e5197ee71983b6ec9d13dbd34374f27
Saurabhkha1 <saurabhkhandelwaltw@gmail.com>
2024-06-21 18:42:06 +0530

    Remove everything

--- android/app/src/main/kotlin/com/example/test_innoventure/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.test_innoventure
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/feature/home/domain/repositories/home_repository.dart
+++
@@ -1,5 +0,0 @@
-import 'package:test_innoventure/feature/home/domain/entities/home_comment_model.dart';
-
-abstract class HomeRepository {
-  Future<List<HomeCommentModel>> fetchItems();
-}
