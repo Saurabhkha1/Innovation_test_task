384d3e06f2d8f717e2bef31995c308ea86adcc4d
Saurabhkha1 <saurabhkhandelwaltw@gmail.com>
2024-06-21 18:59:47 +0530

    Refactor UI

--- lib/feature/detail_view/presentation/pages/detail_page.dart
+++ lib/feature/detail_view/presentation/pages/detail_page.dart
@@ -13,26 +13,38 @@ class DetailPage extends StatelessWidget {
         title: const Text('Detail Page'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Text('Id: ${item.id}'),
-          const SizedBox(
-            height: 10,
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          elevation: 2,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
           ),
-          Text('Post Id: ${item.postId}'),
-          const SizedBox(
-            height: 10,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('Id: ${item.id}'),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text('Post Id: ${item.postId}'),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text('Name: ${item.name}'),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text('Email: ${item.email}'),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text('Description: ${item.body}')
+                ]),
           ),
-          Text('Name: ${item.name}'),
-          const SizedBox(
-            height: 10,
-          ),
-          Text('Email: ${item.email}'),
-          const SizedBox(
-            height: 10,
-          ),
-          Text('Description: ${item.body}')
-        ]),
+        ),
       ),
     );
   }
--- lib/feature/home/presentation/pages/home_page.dart
+++ lib/feature/home/presentation/pages/home_page.dart
@@ -33,22 +33,37 @@ class HomePage extends StatelessWidget {
                 itemCount: state.items.length,
                 itemBuilder: (context, index) {
                   final item = state.items[index];
-                  return ListTile(
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Id: ${item.postId.toString()}"),
-                        Text("Name: ${item.name ?? ""}")
-                      ],
-                    ),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => DetailPage(item: item),
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      color: index % 2 == 0
+                          ? Colors.white
+                          : Colors.grey.withOpacity(0.16),
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Id: ${item.id.toString()}"),
+                              Text("Name: ${item.name ?? ""}")
+                            ],
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DetailPage(item: item),
+                              ),
+                            );
+                          },
                         ),
-                      );
-                    },
+                      ),
+                    ),
                   );
                 },
               );
